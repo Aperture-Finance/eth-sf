6fc4b5bc22786984f92f482ade75eca634ccb5b0
Clique <gaohan137@gmail.com>
2022-11-04 21:49:42 -0700

    Add more details to the vault contract

--- smart_contracts/contracts/UniV3PDNVault.sol
+++ smart_contracts/contracts/UniV3PDNVault.sol
@@ -1,25 +1,59 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.16 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/IHomoraPDN.sol";
+
 contract UniV3PDNVault {
-    address stableToken;
-    address assetToken;
+    using SafeERC20 for IERC20;
+
+    PairInfo pairInfo;
     address bank;
-    uint256 totalShareAmount;
+    // Homora position id.
+    uint256 pid;
+
+    uint256 leverageLevel;
+    uint256 public targetDebtRatio; // target debt ratio * 10000, 92% -> 9200
+    uint256 public debtRatioWidth;
+
+    uint256 public minDebtRatio; // minimum debt ratio * 10000
+    uint256 public maxDebtRatio; // maximum debt ratio * 10000
+    uint256 public deltaThreshold; // delta deviation threshold in percentage * 10000
 
     constructor(
-        address _stableToken,
-        address _assetToken,
+        address stableToken,
+        address assetToken,
         address _bank
     ) payable {
-        stableToken = _stableToken;
-        assetToken = _assetToken;
+        pairInfo.stableToken = stableToken;
+        pairInfo.assetToken = assetToken;
         bank = _bank;
     }
 
-    function deposit(uint256 amount) public {}
+    function setConfig(uint256 _leverageLevel, uint256 _debtRatioWidth)
+        external
+    {
+        leverageLevel = _leverageLevel;
+        debtRatioWidth = _debtRatioWidth;
+    }
 
-    function withfraw(uint256 amount) public {}
+    function deposit(uint256 stableDepositAmount) public {
+        if (stableDepositAmount > 0) {
+            IERC20(pairInfo.stableToken).safeTransferFrom(
+                msg.sender,
+                address(this),
+                stableDepositAmount
+            );
+        }
+
+        // Call library to finish core deposit function.
+        uint256 pidAfter = 20; // Function should return actual pid from Homora.
+        pid = pid == pidAfter ? pid : pidAfter;
+    }
+
+    function withfraw(uint256 amount) public {
+        // Call library to finish core withdraw function.
+    }
 
     function rebalance() public {}
 
